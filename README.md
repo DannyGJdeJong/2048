33a71d65fb44cc6d3b924e2303f75964671e6191
DannyGJdeJong <dannygjdejong@gmail.com>
2020-08-21 14:29:55 +0200

    Added deploy script and updated README

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # 2048
-Simple version of 2048 with variable playing size
+Simple version of 2048 with variable playing size.
+
+Just a little practise project I made to get started with TypeScript and React.
+
+Available on https://2048.denni.dev/.
--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,6 @@ const { generateConfig } = require('gatsby-plugin-ts-config');
 const { resolve } = require('path');
 
 module.exports = generateConfig({
-  configDir: resolve(__dirname, 'gatsby'),
+  configDir: resolve(__dirname, '.gatsby'),
   babel: true
 });
